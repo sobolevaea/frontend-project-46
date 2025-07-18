98dda60301bfa7767bd253bbda6c248623fece6b
sobolevaea <liziaka@mail.ru>
2025-07-18 20:03:08 +0300

    fix gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,10 @@ import { program } from 'commander'
 
 program
   .name('gendiff')
+  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse()
