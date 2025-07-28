8746561effa7686b26409de4ce0f9e7b4afa5bc8
sobolevaea <liziaka@mail.ru>
2025-07-28 18:09:33 +0300

    fix: edit index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+import genDiff from '../src/index.js'
+
+test('json format', () => {
+  const filepath1 = `${__dirname}/../__fixtures__/file1.json`
+  const filepath2 = `${__dirname}/../__fixtures__/file2.json`
+
+  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+  expect(genDiff(filepath1, filepath2)).toBe(expected)
+})
