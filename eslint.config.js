3fcd0eb0f843f64a574afb1248d21d4e0aa268d5
sobolevaea <liziaka@mail.ru>
2025-07-28 18:14:03 +0300

    fix: add jest globals to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,5 @@ import stylistic from '@stylistic/eslint-plugin'
 export default defineConfig([
   stylistic.configs.recommended,
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: { ...globals.node, ...globals.jest } } },
 ])
