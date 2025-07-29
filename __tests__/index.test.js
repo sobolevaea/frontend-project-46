00c679fef9d5c0b33fe87ab65621eef60a15793b
sobolevaea <liziaka@mail.ru>
2025-07-29 12:21:09 +0300

    feat: add yml format test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,3 +13,11 @@ test('json format', () => {
   const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
   expect(genDiff(filepath1, filepath2)).toBe(expected)
 })
+
+test('yaml format', () => {
+  const filepath1 = `${__dirname}/../__fixtures__/file1.yml`
+  const filepath2 = `${__dirname}/../__fixtures__/file2.yml`
+
+  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+  expect(genDiff(filepath1, filepath2)).toBe(expected)
+})
