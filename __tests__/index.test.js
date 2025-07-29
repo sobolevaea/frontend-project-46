b62455a94bea8420aef82b88e0cfe92d7b6a7681
sobolevaea <liziaka@mail.ru>
2025-07-29 14:49:24 +0300

    fix: update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,18 +7,20 @@ const __dirname = path.dirname(__filename)
 
 import genDiff from '../src/index.js'
 
-test('json format', () => {
-  const filepath1 = `${__dirname}/../__fixtures__/file1.json`
-  const filepath2 = `${__dirname}/../__fixtures__/file2.json`
+const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
+const expected = readFile('result.txt')
 
-  const expected = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString()
+test('json format', () => {
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json')
+
   expect(genDiff(filepath1, filepath2)).toBe(expected)
 })
 
 test('yaml format', () => {
-  const filepath1 = `${__dirname}/../__fixtures__/file1.yml`
-  const filepath2 = `${__dirname}/../__fixtures__/file2.yml`
+  const filepath1 = getFixturePath('file1.yml')
+  const filepath2 = getFixturePath('file2.yml')
 
-  const expected = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString()
   expect(genDiff(filepath1, filepath2)).toBe(expected)
 })
