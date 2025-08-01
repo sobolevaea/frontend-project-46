0f420e136218ce998ccff05a11d33336b04be8e7
sobolevaea <liziaka@mail.ru>
2025-08-01 21:41:39 +0300

    fix: move stylish formatter

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -2,14 +2,18 @@ import _ from 'lodash'
 
 const builder = (lines, bracketIndent) => ['{', ...lines, `${bracketIndent}}`].join('\n')
 
+const calcIndents = (depth, spacesCount, replacer) => {
+  const indentSize = depth * spacesCount
+  const currentIndent = replacer.repeat(indentSize)
+  const bracketIndent = replacer.repeat(indentSize - spacesCount)
+  return [currentIndent, bracketIndent]
+}
+
 const iter = (currentValue, depth, spacesCount, replacer = ' ') => {
   if (!_.isObject(currentValue)) {
     return String(currentValue)
   }
-
-  const indentSize = depth * spacesCount
-  const currentIndent = replacer.repeat(indentSize)
-  const bracketIndent = replacer.repeat(indentSize - spacesCount)
+  const [currentIndent, bracketIndent] = calcIndents(depth, spacesCount, replacer)
   const lines = Object
     .entries(currentValue)
     .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1, spacesCount, replacer)}`)
@@ -17,29 +21,26 @@ const iter = (currentValue, depth, spacesCount, replacer = ' ') => {
   return builder(lines, bracketIndent)
 }
 
-export const formatStylish = (tree, depth = 1) => {
+export default formatStylish = (tree, depth = 1) => {
   const spacesCount = 4
   const replacer = ' '
 
-  const indentSize = depth * spacesCount
-  const currentIndent = replacer.repeat(indentSize)
-  const bracketIndent = replacer.repeat(indentSize - spacesCount)
-  const symbolIndent = replacer.repeat(indentSize - 2)
+  const [currentIndent, bracketIndent] = calcIndents(depth, spacesCount, replacer)
 
   const lines = tree.map((el) => {
     switch (el.type) {
       case 'nested':
         return `${currentIndent}${el.key}: ${formatStylish(el.children, depth + 1)}`
       case 'added':
-        return `${symbolIndent}+ ${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`
+        return `${bracketIndent}  + ${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`
       case 'unchanged':
         return `${currentIndent}${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`
       case 'deleted':
-        return `${symbolIndent}- ${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`
+        return `${bracketIndent}  - ${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`
       case 'changed':
         return [
-          `${symbolIndent}- ${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`,
-          `${symbolIndent}+ ${el.key}: ${iter(el.value2, depth + 1, spacesCount, replacer)}`,
+          `${bracketIndent}  - ${el.key}: ${iter(el.value1, depth + 1, spacesCount, replacer)}`,
+          `${bracketIndent}  + ${el.key}: ${iter(el.value2, depth + 1, spacesCount, replacer)}`,
         ].join('\n')
       default:
         throw new Error(`Unknown type: ${el.type}`)
