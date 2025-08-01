d04d167e2de55ed56b33517c61a23d6c25e87749
sobolevaea <liziaka@mail.ru>
2025-08-01 22:03:39 +0300

    fix: fix format option in bin/index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,8 @@ program
   .arguments('<filepath1> <filepath2')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2))
+    const formatName = program.opts().format
+    console.log(genDiff(filepath1, filepath2, formatName))
   })
 
 program.parse()
