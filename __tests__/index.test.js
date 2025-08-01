e555dca102d03315469249a28a39e3a4894192b3
sobolevaea <liziaka@mail.ru>
2025-08-01 21:39:35 +0300

    feat: add new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,18 +9,21 @@ import genDiff from '../src/index.js'
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
-const expected = readFile('result.txt')
+const stylish = readFile('stylish.txt')
+const plain = readFile('plain.txt')
 
 test('json format', () => {
   const filepath1 = getFixturePath('file1.json')
   const filepath2 = getFixturePath('file2.json')
 
-  expect(genDiff(filepath1, filepath2)).toBe(expected)
+  expect(genDiff(filepath1, filepath2)).toBe(stylish)
+  expect(genDiff(filepath1, filepath2, 'plain')).toBe(plain)
 })
 
 test('yaml format', () => {
   const filepath1 = getFixturePath('file1.yml')
   const filepath2 = getFixturePath('file2.yml')
 
-  expect(genDiff(filepath1, filepath2)).toBe(expected)
+  expect(genDiff(filepath1, filepath2)).toBe(stylish)
+  expect(genDiff(filepath1, filepath2, 'plain')).toBe(plain)
 })
